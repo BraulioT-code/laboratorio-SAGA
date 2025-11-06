45f72b0a7185c27100ceece9ae73f5911f799692
Braulio Tovar <bratoli1998@gmail.com>
2025-11-05 19:47:24 -0500

    Creación laboratorio

---
+++ src/test/java/com/travel/laboratoriosaga/LaboratorioSagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.laboratoriosaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratorioSagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
